5a1067f8803b25c1fb3f149f20d30bdf316069a6
Joseph F. Klaszky <jfk99@grep.cs.rutgers.edu>
2016-10-01 11:22:16 -0400

    just need input validation on the right side of input

--- fourth.c
+++ fourth.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #define MAX 1000
+
+void *malloc(size_t size);
+void free(void *ptr);
+int fscanf(FILE *stream, const char *format, ...);
+int strcmp(const char *string1, const char *string2);
 
 struct hashItem
 {
@@ -48,14 +52,14 @@ void insert(int value)
 	item->data = value;
 
 	int index = hash(value);
-	
-	if(hashArr[index] != NULL && hashArr[index]->data == value)
-	{
-		return;
-	}
-		
+			
 	while(hashArr[index] != NULL)
 	{
+		if(hashArr[index]->data == value)
+		{
+			return;
+		}
+
 		index++;
 
 		if(index == MAX)
@@ -64,6 +68,7 @@ void insert(int value)
 		}
 	}
 	hashArr[index] = item;
+	printf("inserted\n");
 }
 
 //struct DataItem* del(struct HashItem *item){}
@@ -72,12 +77,47 @@ void insert(int value)
 
 int main(int argc, char *argv[])
 {
-	insert(42);
-	insert(42);
-	insert(1042);
-	search(1042);
-	search(42);
-	search(69);
+	FILE *file_pointer;
+	char *mode = "r";
+	if(argc == 1)
+	{
+		printf("error\n");
+		return 0;
+	}
+	else
+	{
+		file_pointer = fopen(argv[1], mode);
+		if(file_pointer == NULL)
+		{
+			printf("error\n");
+			return 0;
+		}
+	}
+
+	char text[5];
+	int value;
+	while(fscanf(file_pointer, "%s %d", text, &value) != EOF)
+	{
+		if( strcmp(text, "i") != 0 && strcmp(text, "s"))
+		{
+			printf("error\n");
+		}
+		else if(!strcmp(text, "i"))
+		{
+			insert(value);
+		}
+		else if(!strcmp(text, "s"))
+		{
+			search(value);
+		}
+	}
+	//insert(42);
+	//insert(42);
+	//insert(43);
+	//insert(1042);
+	//search(1042);
+	//search(42);
+	//search(69);
 	return 0;
 }
 
