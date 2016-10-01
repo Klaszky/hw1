5f1a89db25637b8033f4cf2ab3529c9a6fceff7c
Joseph F. Klaszky <jfk99@grep.cs.rutgers.edu>
2016-10-01 00:28:18 -0400

    almost finished, need to do file i/o and input validation

--- fourth.c
+++ fourth.c
@@ -1,29 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 #define MAX 1000
 
-struct HashItem
+struct hashItem
 {
 	int data;
-	int key;
 };
 
-struct HashItem *hashArr[MAX];
-struct HashItem *dItem;
-struct HashItem *item;
+struct hashItem *hashArr[MAX];
+struct hashItem *dItem;
+struct hashItem *item;
 
-int hashCode(int key)
+int hash(int data)
 {
-	return key % MAX;
+	return data % MAX;
 }
 
-//struct HashItem *search(int key){}
+void search(int value)
+{
+	int index = hash(value);
+	
+	while(hashArr[index] != NULL)
+	{
+		if(hashArr[index]->data == value)
+		{
+			printf("present\n");
+			return;
+		}
 
-void insert(int key, int data)
-{}
+		index++;
+
+		if(index == MAX)
+		{
+			index = 0;
+		}
+	}
+
+	printf("absent\n");
+	return;
+}
+
+void insert(int value)
+{
+	struct hashItem *item = (struct hashItem*) malloc(sizeof(struct hashItem));
+	item->data = value;
+
+	int index = hash(value);
+	
+	if(hashArr[index] != NULL && hashArr[index]->data == value)
+	{
+		return;
+	}
+		
+	while(hashArr[index] != NULL)
+	{
+		index++;
+
+		if(index == MAX)
+		{
+			index = 0;
+		}
+	}
+	hashArr[index] = item;
+}
 
 //struct DataItem* del(struct HashItem *item){}
 
@@ -31,6 +72,12 @@ void insert(int key, int data)
 
 int main(int argc, char *argv[])
 {
+	insert(42);
+	insert(42);
+	insert(1042);
+	search(1042);
+	search(42);
+	search(69);
 	return 0;
 }
 
